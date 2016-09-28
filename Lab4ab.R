8ac6e300823b5b93aabbed892b0060060e1893f1
Professor333 <rstikele@asu.edu>
2016-09-28 10:21:23 -0700

    Adds some titles to graphs, dataset 2 transformed code was missing, it has now been added

--- Lab4ab.R
+++ Lab4ab.R
@@ -17,17 +17,17 @@ sample.size.a= aggregate(.~size1, data=a, length)
 sample.size.a=sample.size.a[order(sample.size.a$size1),]
 
 #Step 1, illustrates that dataset 1 is visually non-linear
-boxplot(rich1~size12,xlab="Sizes",ylab="Species")
+boxplot(rich1~size12,xlab="Sizes",ylab="Species", main = "dataset 1")
 
 out11=aov(rich1~size12)
 summary(out11) #Size is significantly different among samples
 TukeyHSD(x=out11, 'size12', conf.level = 0.95) #Shows which sizes are significantly different from each other
 
-qqPlot(out11$resid,ylab="Z value") #NPP test, looks pretty normal, but there is some slight tailing at the ends
+qqPlot(out11$resid,ylab="Z value", main = "dataset 1") #NPP test, looks pretty normal, but there is some slight tailing at the ends
 shapiro.test(out11$resid) #Results indicate that the data is normal
 
 par(mfrow = c(2, 2))  
-plot(out11) #Everything looks about normal, but scale-location looks a little non-linear. That test assumes homoscedasticity, and if the assumption is met, then the line is flat, but if it is not met, then the line will be curved some way.
+plot(out11, main = "dataset 1") #Everything looks about normal, but scale-location looks a little non-linear. That test assumes homoscedasticity, and if the assumption is met, then the line is flat, but if it is not met, then the line will be curved some way.
 
 bartlett.test(rich1~size12)#Results indicate unequal variance among samples
 
@@ -121,16 +121,16 @@ size22<-factor(size2,levels=c("10","20","30","40",">50"))
 sample.size.b= aggregate(.~size2, data=b, length)
 sample.size.b=sample.size.b[order(sample.size.b$size2),]
 
-boxplot(rich2~size22,xlab="Sizes",ylab="Species")
+boxplot(rich2~size22,xlab="Sizes",ylab="Species", main = "dataset 2")
 
 out22=aov(rich2~size22)
 summary(out22)
 
-qqPlot(out22$resid,ylab="Z value")
+qqPlot(out22$resid,ylab="Z value", main = "dataset 2")
 shapiro.test(out22$resid)
 
 par(mfrow = c(2, 2))  
-plot(out22)
+plot(out22, main = "dataset 2")
 
 bartlett.test(rich2~size22)
 
@@ -168,4 +168,38 @@ cohen.f2=sqrt((SSG/N) / (SSE/(N-k))) #Equivalent to by hand
 
 #Dataset 2 Transformed, sqrt
 
+dev.off()
+sqrtrich2<-sqrt(rich2)
+boxplot(sqrtrich2~size22,xlab="Sizes",ylab="Species") #Visually linear
 
+out22sqrt=aov(sqrtrich2~size22)
+summary(out22sqrt) #Size is still significantly different among samples
+TukeyHSD(x=out22sqrt, 'size22', conf.level = 0.95) #Shows which sizes are significantly different from each other
+
+qqPlot(out22sqrt$resid,ylab="Z value") #NPP test, normal
+shapiro.test(out22sqrt$resid) #Results indicate that the data is normal; p>0.05 is a normal dataset
+
+par(mfrow = c(2, 2))  
+plot(out22sqrt) #Everything looks about normal, but scale-location looks a little non-linear. That test assumes homoscedasticity, and if the assumption is met, then the line is flat, but if it is not met, then the line will be curved some way. This is homoscedastic.
+#Gaps between variances are objectively smaller though
+
+bartlett.test(sqrtrich2~size22) #p-value less than 0.05 means unequal variance
+#Equal variance here
+
+#ANOVA test is the final test to run given the stat tree
+aov(sqrtrich2~size22)
+
+pairwise.t.test(rich2,size22,p.adj="bonf")#Good way to see pairwise differences by group on raw data
+pairwise.t.test(sqrtrich2,size22,p.adj="bonf") #Good way to see pairwise differences by group on transformed data
+
+out2<-HSD.test(aov(rich2~size22),"size22",group=T,console=T) #Compare sig differences between raw and transformed data
+sqrtout2<-HSD.test(aov(sqrtrich2~size22),"size22",group=T,console=T)
+SNK.test(aov(rich2~size22),"size22",group=T,console=T)
+SNK.test(aov(sqrtrich2~size22),"size22",group=T,console=T)
+
+output3=lm(sqrtrich2~size22)
+SSG3=anova(output3)[1,2]
+SSE3=anova(output3)[2,2]
+cohen.f4=SSG3/SSE3
+pwr.anova.test(k=5, n= 20, f=cohen.f4, sig.level = 0.05, power = NULL)
+#Experimental test results are very strong
