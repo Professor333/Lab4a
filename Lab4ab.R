ebf1af003ad0b1572f2a9d1a62c3675449ce418a
Professor333 <rstikele@asu.edu>
2016-09-27 17:07:34 -0700

    Contains analysis of Raw dataset 1 and 2, as well as transformed dataset 1 and 2. Lot of information here.

--- Lab4ab.R
+++ Lab4ab.R
@@ -11,24 +11,30 @@ size12<-factor(size1,levels=c("10","20","30","40",">50"))
 library(car)
 library(ggplot2)
 library(pwr)
+library(agricolae)
 
 sample.size.a= aggregate(.~size1, data=a, length)
 sample.size.a=sample.size.a[order(sample.size.a$size1),]
 
+#Step 1, illustrates that dataset 1 is visually non-linear
 boxplot(rich1~size12,xlab="Sizes",ylab="Species")
 
 out11=aov(rich1~size12)
-summary(out11)
+summary(out11) #Size is significantly different among samples
+TukeyHSD(x=out11, 'size12', conf.level = 0.95) #Shows which sizes are significantly different from each other
 
-qqPlot(out11$resid,ylab="Z value")
-shapiro.test(out11$resid)
+qqPlot(out11$resid,ylab="Z value") #NPP test, looks pretty normal, but there is some slight tailing at the ends
+shapiro.test(out11$resid) #Results indicate that the data is normal
 
 par(mfrow = c(2, 2))  
-plot(out11)
+plot(out11) #Everything looks about normal, but scale-location looks a little non-linear. That test assumes homoscedasticity, and if the assumption is met, then the line is flat, but if it is not met, then the line will be curved some way.
 
-bartlett.test(rich1~size12)
+bartlett.test(rich1~size12)#Results indicate unequal variance among samples
 
-anova(out11)
+anova(out11) #Confirms that there is a difference between the groups
+
+out11.mod = data.frame(Fitted = fitted(out11), Residuals = resid(out11), Treatment = size12)
+ggplot(out11.mod, aes(Fitted, Residuals, colour = Treatment)) + geom_point() #Confirms visually variation diffences among samples by their visual spread from each other
 
 square.between= function(x) (mean(x) - grand.mean.a)^2
 group.mean.a<- aggregate(.~size1, data=a, mean)
@@ -59,11 +65,51 @@ SSE=anova(output1)[2,2]
 cohen.f=SSG/SSE
 
 cohen.f2=sqrt((SSG/N) / (SSE/(N-k))) #Equivalent to by hand
+pwr.anova.test(k=5, n= 20, f=cohen.f, sig.level = 0.05, power = NULL)
+
+#Sqrt Transformation, does not fix unequal variance
+dev.off()
+sqrtrich1<-sqrt(rich1)
+boxplot(sqrtrich1~size12,xlab="Sizes",ylab="Species") #Still visually non-linear
+
+out11sqrt=aov(sqrtrich1~size12)
+summary(out11sqrt) #Size is still significantly different among samples
+TukeyHSD(x=out11sqrt, 'size12', conf.level = 0.95) #Shows which sizes are significantly different from each other
+
+qqPlot(out11sqrt$resid,ylab="Z value") #NPP test, looks pretty normal, but the tranformation created an outlier
+shapiro.test(out11sqrt$resid) #Results indicate that the data is normal; p>0.05 is a normal dataset
+
+par(mfrow = c(2, 2))  
+plot(out11sqrt) #Everything looks about normal, but scale-location looks a little non-linear. That test assumes homoscedasticity, and if the assumption is met, then the line is flat, but if it is not met, then the line will be curved some way.
+#QQplot now has 2 outliers
+#Gaps between variances are objectively smaller though
+
+bartlett.test(sqrtrich1~size12) #p-value less than 0.05 means unequal variance
+#Unequal variance here, still
+
+#Welch's test is the final test to run given the stat tree
+
+t.test(sqrtrich1, y=NULL, c(alternative = "greater"), paired=FALSE, var.equal= F)
+#True mean is greater than zero
+pairwise.t.test(rich1,size12,p.adj="bonf")#Good way to see pairwise differences by group on raw data
+pairwise.t.test(sqrtrich1,size12,p.adj="bonf") #Good way to see pairwise differences by group on transformed data
+dev.off()
+
+out1<-HSD.test(aov(rich1~size12),"size12",group=T,console=T) #Compare sig differences between raw and transformed data
+sqrtout1<-HSD.test(aov(sqrtrich1~size12),"size12",group=T,console=T)
+SNK.test(aov(rich1~size12),"size12",group=T,console=T)
+SNK.test(aov(sqrtrich1~size12),"size12",group=T,console=T)
+
+output2=lm(sqrtrich1~size12)
+SSG2=anova(output2)[1,2]
+SSE2=anova(output2)[2,2]
+cohen.f3=SSG2/SSE2
+pwr.anova.test(k=5, n= 20, f=cohen.f3, sig.level = 0.05, power = NULL)
+#Experimental test results are very strong
 
 
 
-
-#For dataset 2
+#For dataset 2 untransformed
 b=read.csv("lab4data2.csv", header = T)
 attach(b)
 names(b)
@@ -119,5 +165,7 @@ SSE=anova(output1)[2,2]
 cohen.f=SSG/SSE
 
 cohen.f2=sqrt((SSG/N) / (SSE/(N-k))) #Equivalent to by hand
+
+#Dataset 2 Transformed, sqrt
 
 
